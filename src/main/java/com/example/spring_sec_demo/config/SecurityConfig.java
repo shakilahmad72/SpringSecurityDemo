e2e167cf50025ef12cc638860399843ae2e82589
shakilahmad72 <iamshakilahmad27@gmail.com>
2026-09-23 22:01:29 +0530

    initial commit

--- src/main/java/com/example/spring_sec_demo/config/SecurityConfig.java
+++ src/main/java/com/example/spring_sec_demo/config/SecurityConfig.java
@@ -3,18 +3,18 @@ package com.example.spring_sec_demo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +22,9 @@ public class SecurityConfig {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public AuthenticationProvider authProvider() {
@@ -46,12 +49,19 @@ public class SecurityConfig {
 
         http
                 .csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/register", "/login")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
+        return config.getAuthenticationManager();
     }
 
 //    @Bean
--- src/main/java/com/example/spring_sec_demo/controller/HelloController.java
+++ src/main/java/com/example/spring_sec_demo/controller/HelloController.java
@@ -9,11 +9,11 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String greet(HttpServletRequest request) {
-        return "Hello World " + request.getSession().getId();
+        return "Hello World ";
     }
 
     @GetMapping("/about")
     public String about(HttpServletRequest request) {
-        return "Example " + request.getSession().getId();
+        return "Example ";
     }
 }
